b4572c33be77dd42bca83eea052e2bf1160afcb6
chamc <chamc@DESKTOP-TLUDASV>
2023-03-18 22:06:18 +0900

    ㅇㅇ

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("정상 실행됨!!");
 		new GraphicsMainDraw();
+		System.out.println("정상 실행됨.");
 	}
 
 }

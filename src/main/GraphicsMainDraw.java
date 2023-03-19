18007041ce3e5794f86ae0e4b6cfb0c870bcbf3a
chamc <chamc@DESKTOP-TLUDASV>
2023-03-19 19:47:09 +0900

    20230319 - 장애물(선인장, 새) 추가 - 플레이어 달리는 동작 & 웅크리는 동작 추가 - 피격 판정 추가 - 점수 표시, 최고점수 저장 기능 추가 - 배경으로 바닥과 움직이는 구름 추가 - 시작메뉴 및 다시하기 메뉴 추가

--- src/main/GraphicsMainDraw.java
+++ src/main/GraphicsMainDraw.java
@@ -1,58 +1,94 @@
 package main;
 
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Panel;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 class Define {
-	final static Double GRAVITY = 0.098;	// 중력가속도 (1/100초)
-	final static int GROUND = 360;			// 지면 Y좌표
-	final static int RUNLOCATION = 160;		// 달리는 X좌표
+	final static Double GRAVITY = 0.098; // 중력가속도 (1/100초)
+	final static int GROUND = 250; // 지면 Y좌표
+	final static int RUNLOCATION = 100; // 달리는 X좌표
 
 }
 
 public class GraphicsMainDraw extends JFrame {
 	// MyPanel 객체 생성
 	private MyPanel panel = new MyPanel();
-	private int counter;
+	private int counter, maxscore = 0;
+
+	// 게임 시작 & 오버 메시지
+	private String mainMessage = "";
+	private String mainMessage2 = "";
 
 	// 더블 버퍼링 변수
 	private Image bufferImage;
 	private Graphics screenGraphic;
 
 	// 플레이어 변수
-	private int playerX = Define.RUNLOCATION, playerY = Define.GROUND; // 좌표 (초기좌표)
-	private ImageIcon playerIcon = new ImageIcon("images/playerImg.png"); // 아이콘 불러오기
-	private Image playerImg = playerIcon.getImage(); // 이미지 지정
+	private int playerX = Define.RUNLOCATION, playerY = Define.GROUND, playerAction = 1; // 좌표 (초기좌표)
+	private int playerSizeX = 50, playerSizeY = 50;
+	private ImageIcon playerIcon0 = new ImageIcon("images/run0.png"); // 아이콘 불러오기
+	private ImageIcon playerIcon1 = new ImageIcon("images/run1.png"); // 아이콘 불러오기
+	private ImageIcon playerIcon2 = new ImageIcon("images/run2.png"); // 아이콘 불러오기
+	private Image playerImg = playerIcon1.getImage(); // 이미지 지정
+	private boolean hit = false, retry = false;
+
 	// 플레이어 키 입력 변수
 	private boolean up = false, down = false, releaseUp = true;
 	private int vt = 0, vf = 0;
 
+	// 바닥 변수
+	private ImageIcon groundIcon = new ImageIcon("images/floor.png"); // 아이콘 불러오기
+	private Image groundImg = groundIcon.getImage(); // 이미지 지정
+
+	// 구름 변수
+	private ImageIcon cloudIcon = new ImageIcon("images/cloud_cut.png"); // 아이콘 불러오기
+	private Image cloudImg = cloudIcon.getImage(); // 이미지 지정
+	private ArrayList<cloud> cloudList = new ArrayList<>();
+
+	// 선인장 변수
+	private ImageIcon cactusIcon1 = new ImageIcon("images/g1.png"); // 아이콘 불러오기
+	private ImageIcon cactusIcon2 = new ImageIcon("images/g2.png"); // 아이콘 불러오기
+	private ImageIcon cactusIcon3 = new ImageIcon("images/g3.png"); // 아이콘 불러오기
+	private ImageIcon cactusIcon4 = new ImageIcon("images/g4.png"); // 아이콘 불러오기
+	private ImageIcon cactusIcon5 = new ImageIcon("images/bird.png"); // 아이콘 불러오기
+	private ArrayList<cactus> cactusList = new ArrayList<>();
+	private cactus c1 = new cactus();
+
+	private double accel = 0; // 1/500배
+
 	// 화면크기
 	private final int SCREENX = 1000;
-	private final int SCREENY = 600;
+	private final int SCREENY = 400;
 
 	// 클래스 :
 	// 키 입력 클래스
 	public class AL extends KeyAdapter {
 		public void keyPressed(KeyEvent e) {
 			int keyCode = e.getKeyCode();
-			if (keyCode == e.VK_DOWN)
+			if (keyCode == e.VK_DOWN) {
 				down = true;
+			}
 			if (keyCode == e.VK_UP)
 				up = true;
+			if (keyCode == e.VK_ENTER && retry == false && hit == true)
+				retry = true;
 
 		}
 
 		public void keyReleased(KeyEvent e) {
 			int keyCode = e.getKeyCode();
-			if (keyCode == e.VK_DOWN)
+			if (keyCode == e.VK_DOWN) {
 				down = false;
+			}
 			if (keyCode == e.VK_UP) {
 				up = false;
 			}
@@ -70,9 +106,21 @@ public class GraphicsMainDraw extends JFrame {
 		setVisible(true);
 		setLocationRelativeTo(null);
 
+		while (up == false) {
+			try {
+				Thread.sleep(10);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			mainMessage = "Dino Game";
+			mainMessage2 = "Use 'Up' and 'Down' key";
+			
+		}
+		init();
+
 		// 무한 루프문 :
 		// 게임 시나리오 작성 공간
-		while (true) {
+		while (counter < 50 && hit == false) {
 			try {
 				Thread.sleep(10);
 			} catch (Exception e) {
@@ -81,12 +129,205 @@ public class GraphicsMainDraw extends JFrame {
 
 			counter++;
 
-			keyProcess(); // 플레이어 이동&공격 프로세스
-			MoveProcess();
-
-			counter = counter > 99999999 ? 0 : counter; // 카운터 초기화
+			GenerateProcess(); // 유닛 생성 프로세스
+			MoveProcess(); // 유닛 이동 프로세스
+			removeProcess();
+			checkProcess();
 
 		}
+
+		while (true) {
+			try {
+				Thread.sleep(10);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			accel = accel < 1 ? accel + 0.0005 : accel;
+			counter++;
+
+			GenerateProcess(); // 유닛 생성 프로세스
+			keyProcess(); // 플레이어 이동&공격 프로세스
+			MoveProcess(); // 유닛 이동 프로세스
+			checkProcess();
+			removeProcess();
+
+			counter = counter > 99999 ? 99999 : counter; // 카운터 고정
+
+			// 피격된 경우
+			// 정지
+			while (hit) {
+				try {
+					Thread.sleep(10);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				mainMessage = "GAVE OVER";
+				mainMessage2 = "Press ENTER to continue";
+
+				if (retry) {
+					retry = false;
+					maxscore = maxscore < counter ? counter : maxscore;
+					init();
+					hit = false;
+
+				}
+			}
+
+		}
+
+	}
+
+	// 메소드 : 초기화
+	// 속도 초기화
+	// 선인장 초기화
+	public void init() {
+		// 플레이어 지면으로 이동
+		playerY = Define.GROUND;
+
+		// 시작 점프
+		vf = 9;
+		vt = 0;
+
+		// 시간(점수) 초기화
+		counter = 0;
+
+		// 가속 초기화
+		accel = 0;
+
+		// 선인장 초기화
+		cactusList.clear();
+		new cloud(1040, (int) (Math.random() * 71 + 50), cloudImg);
+		cactus.ccounter = 160;
+		mainMessage = "";
+		mainMessage2 = "";
+
+	}
+
+	// 클래스 : 선인장 클래스
+	// 선인장 이동시키고, 다음 장애물 생성위치 결정
+	class cactus {
+		private int x, y, sizeX, sizeY;
+		private Image cactusImg;
+		private static int nextX, nextY, ccounter, type;
+
+		public cactus() {
+
+		}
+
+		public cactus(int x, int y, int sizeX, int sizeY, Image cactusImg) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.sizeX = sizeX;
+			this.sizeY = sizeY;
+			this.cactusImg = cactusImg;
+
+			cactusList.add(this);
+
+			// 생성위치
+			cactus.ccounter = (int) ((Math.random() * 25 + this.sizeX / 2 + 10) * (2 - accel));
+			cactus.type = (int) (Math.random() * 7) + 0;
+		}
+
+		void move() {
+			this.x -= (14 - 7 + (int) (7 * accel));
+
+		}
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
+
+		public Image getCactusImg() {
+			return cactusImg;
+		}
+
+		public int getSizeX() {
+			return sizeX;
+		}
+
+		public void setSizeX(int sizeX) {
+			this.sizeX = sizeX;
+		}
+
+		public int getSizeY() {
+			return sizeY;
+		}
+
+		public void setSizeY(int sizeY) {
+			this.sizeY = sizeY;
+		}
+
+		public void setCactusImg(Image cactusImg) {
+			this.cactusImg = cactusImg;
+		}
+
+	}
+
+	// 클래스 : 구름 클래스
+	// 구름 이동시키고, 다음 구름 생성위치 마련
+	class cloud {
+		private double x, y;
+		private Image cloudImg;
+		private static int nextX, nextY;
+
+		public cloud() {
+
+		}
+
+		public cloud(int x, int y, Image cloudImg) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.cloudImg = cloudImg;
+
+			cloudList.add(this);
+			nextX = (int) (Math.random() * 200 + 1000);
+			nextY = (int) (Math.random() * 81 + 20);
+		}
+
+		void move() {
+			this.x -= 1.2;
+
+		}
+
+		public int getX() {
+			return (int) x;
+		}
+
+		public void setX(double x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return (int) y;
+		}
+
+		public void setY(double y) {
+			this.y = y;
+		}
+
+		public Image getCloudImg() {
+			return cloudImg;
+		}
+
+		public void setCloudImg(Image cloudImg) {
+			this.cloudImg = cloudImg;
+		}
+
 	}
 
 	// 메소드:
@@ -94,30 +335,152 @@ public class GraphicsMainDraw extends JFrame {
 	public void keyProcess() {
 		if (up) {
 			if (releaseUp) {
-				vf = 11;
+				vf = 10;
 				releaseUp = false;
 				vt = 0;
 			}
 		}
+		if (down) {
+			vt = 90;
+		}
+	}
+
+	// 메소드 :
+	// 유닛 생성 메소드
+	public void GenerateProcess() {
+		// 구름 생성 :
+		// 마지막 구름이 x700 좌표를 넘어가면 하나 생성
+		if (cloudList.get(cloudList.size() - 1).getX() < 700) {
+			new cloud(cloud.nextX, cloud.nextY, cloudImg);
+		}
+
+		// 선인장 생성 :
+		//
+		cactus.ccounter--;
+		if (cactus.ccounter <= 0) {
+			switch (cactus.type) {
+			// 긴 선인장 하나
+			case 0: {
+				new cactus(1010, 245, 35, 55, cactusIcon1.getImage());
+				break;
+			}
+
+			// 작은 선인장 하나
+			case 1: {
+				new cactus(1010, 210, 50, 80, cactusIcon1.getImage());
+				break;
+			}
+
+			// 중간 선인장 1개
+			case 2: {
+				new cactus(1010, 230, 45, 70, cactusIcon2.getImage());
+				break;
+			}
+
+			// 작은 선인장 3개
+			case 3: {
+				new cactus(1010, 245, 80, 55, cactusIcon3.getImage());
+				break;
+			}
+
+			// 긴 선인장 여러개
+			case 4: {
+				new cactus(1010, 220, 110, 80, cactusIcon4.getImage());
+				break;
+			}
+			// 낮은 새
+			case 5: {
+				new cactus(1010, 245 - 30, 55, 55, cactusIcon5.getImage());
+				break;
+			}
+			// 낮은 새
+			case 6: {
+				new cactus(1010, 245 - 30 - 60, 50, 55, cactusIcon5.getImage());
+				break;
+			}
+
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + cactus.type);
+			}
+
+		}
+
 	}
 
 	// 메소드 :
 	// 유닛 이동 프로세스
 	public void MoveProcess() {
-		System.out.println("Y 좌표 : " + playerY);
-		System.out.println("중력 가속도 : " + vf);
+		/*
+		 * System.out.println("Y 좌표 : " + playerY); System.out.println("중력 가속도 : " + vf
+		 * + ", vt : " + vt); System.out.println("accel : " + accel + ", cactus 객체 수 : "
+		 * + cactusList.size());
+		 */
 
 		// 플레이어 점프
 		// 최고 높이 : y 184
 		if (vf > 0 || playerY < Define.GROUND) {
-			vf -= (int)(Define.GRAVITY * vt);
+			vf -= (int) (Define.GRAVITY * vt);
 			playerY -= vf;
-			if (playerY - (vf - (int)(Define.GRAVITY * (vt + 1))) > Define.GROUND) {
+			if (playerY - (vf - (int) (Define.GRAVITY * (vt + 1))) >= Define.GROUND) {
 				playerY = Define.GROUND;
 				releaseUp = true;
 			}
 			vt = vt < 12 ? vt + 1 : vt;
-			
+
+		}
+
+		// 플레이어 달리기 이미지 변경
+		if (down) { // 웅크리기
+			playerImg = playerIcon0.getImage();
+		} else if (playerY < Define.GROUND) { // 점프 동작
+			playerImg = playerIcon1.getImage();
+		} else if (counter % 4 == 0) {
+			if (playerAction == 1) {
+				playerImg = playerIcon2.getImage(); // 달리기2
+				playerAction = 2;
+			} else if (playerAction == 2) {
+				playerImg = playerIcon1.getImage(); // 달리기1
+				playerAction = 1;
+			}
+		}
+
+		// 구름 이동
+		for (int i = 0; i < cloudList.size(); i++) {
+			cloudList.get(i).move();
+		}
+
+		// 선인장 이동
+		for (int i = 0; i < cactusList.size(); i++) {
+			cactusList.get(i).move();
+		}
+	}
+
+	// 메소드 : 충돌 체크 프로세스
+	// 플레이어의 충돌 여부 확인
+	public void checkProcess() {
+		for (int i = 0; i < cactusList.size(); i++) {
+			c1 = cactusList.get(i);
+			if (playerX + playerSizeX - 7 > c1.getX() + 5 && playerX + 7 < c1.getX() + c1.getSizeX() - 8
+					&& playerY + playerSizeY > c1.getY() + 4 && playerY + downChecker() < c1.getY() + c1.getSizeX()) {
+				hit = true;
+			}
+		}
+	}
+
+	// 메소드 : 물체 제거 메소드
+	// 화면 나간 물체 제거
+	public void removeProcess() {
+		for (int i = 0; i < cactusList.size(); i++) {
+			if (cactusList.get(i).getX() < -150)
+				cactusList.remove(i);
+		}
+	}
+
+	public int downChecker() {
+		if (down) {
+			return 25;
+		} else {
+			return 0;
 		}
 	}
 
@@ -136,8 +499,30 @@ public class GraphicsMainDraw extends JFrame {
 		// 메소드 : 더블버퍼 그리기
 		// 유닛들 여기에 추가
 		public void paintComponent(Graphics g) {
+			// 구름 그리기
+			for (int i = 0; i < cloudList.size(); i++)
+				g.drawImage(cloudImg, cloudList.get(i).getX(), cloudList.get(i).getY(), 60, 25, this);
+
+			// 선인장 그리기
+			for (int i = 0; i < cactusList.size(); i++)
+				g.drawImage(cactusList.get(i).getCactusImg(), cactusList.get(i).getX(), cactusList.get(i).getY(),
+						cactusList.get(i).getSizeX(), cactusList.get(i).getSizeY(), this);
+
+			// 바닥 그리기
+			g.drawImage(groundImg, 0, 290, 1000, 11, this);
+
 			// 플레이어 그리기
-			g.drawImage(playerImg, playerX, playerY, 60, 60, this);
+			g.drawImage(playerImg, playerX, playerY, playerSizeX, playerSizeY, this);
+
+			// 점수 그리기
+			g.setFont(new Font("맑은 고딕", getFont().PLAIN, 20));
+			g.drawString("score : " + Integer.toString(counter), 770, 20 + 10);
+			g.setFont(new Font("맑은 고딕", getFont().PLAIN, 20));
+			g.drawString("Max score : " + Integer.toString(maxscore), 550, 20 + 10);
+			g.setFont(new Font("맑은 고딕", getFont().BOLD, 35));
+			g.drawString(mainMessage, 390, 140);
+			g.setFont(new Font("맑은 고딕", getFont().PLAIN, 20));
+			g.drawString(mainMessage2, 370, 200);
 
 			repaint();
 		}
